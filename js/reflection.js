48590ed3734214393f6ed9cfc07c718c9d630db6
sushilmainali <mainalis@gmail.com>
2015-03-10 20:00:46 +0000

    Adding project 2

---
+++ js/reflection.js
@@ -0,0 +1,4 @@
+How might using version control make you more confident to make changes that could break something?
+
+version control gives facility to go back to the past commit and extract code from that commit. this gives
+us confident to make changes that could break something.
